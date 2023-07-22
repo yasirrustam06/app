ce9c6fd0dcf3f71ca43e566ca4001ee9835d8df5
yasir ali <yasirrustam06@gmail.com>
2023-07-22 02:22:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -133,111 +133,148 @@
 
 
 
+# import cv2
+# import streamlit as st
+# from PIL import Image
+
+# RESIZE_WIDTH = 500
+# RESIZE_HEIGHT = 300
+
+# def process_uploaded_image(uploaded_file):
+#     if uploaded_file is not None:
+#         image = Image.open(uploaded_file)
+#         # Convert RGBA to RGB if the image has an alpha channel
+#         if image.mode == 'RGBA':
+#             image = image.convert('RGB')
+
+#         image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
+#         image_path = "uploaded_image.jpg"  # Save the resized image to a file
+#         image.save(image_path)
+#         return image_path
+#     return None
+
+# def process_camera_snapshot():
+#     video_capture = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
+#     while video_capture.isOpened():
+#         ret, frame = video_capture.read()
+#         if not ret:
+#             break
+#         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#         image = Image.fromarray(frame)
+#         image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
+#         snapshot_path = "snapshot.jpg"  # Save the resized snapshot image to a file
+#         image.save(snapshot_path)
+#         return snapshot_path
+#     return None
+
+# def main():
+#     # image = Image.open('D:\pythonProject1\MediTi.png')
+#     # st.image(
+#     #     image,
+#     #     width=400,  # Manually adjust the width of the image as per requirement
+#     #     caption="MediTi Logo"  # Add a non-empty label for accessibility
+#     # # )
+
+#     st.title("Image Comparing Streamlit App")
+
+#     col1, col2 = st.columns(2)
+
+#     image_path = None  # Initialize image_path variable
+#     snapshot_path = None  # Initialize snapshot_path variable
+
+#     with col1:
+#         st.subheader("Upload an Image")
+#         uploaded_file = st.file_uploader("&nbsp", type=["jpg", "jpeg", "png"], accept_multiple_files=False, help="",label_visibility="hidden")
+#         if uploaded_file is not None:
+#             image_path = process_uploaded_image(uploaded_file)
+
+#     with col2:
+#         st.subheader("Camera Snapshot")
+#         snapshot_button = st.button("Take Snapshot")
+#         if snapshot_button:
+#             snapshot_path = process_camera_snapshot()
+
+#     # Show both images in a balanced manner
+#     if image_path is not None and snapshot_path is not None:
+#         st.subheader("Images")
+#         col1, col2 = st.columns(2)
+#         with col1:
+#             st.image(image_path, caption="Uploaded Image")
+#         with col2:
+#             st.image(snapshot_path, caption="Snapshot")
+
+#     # Perform image comparison and print the result
+#     if image_path and snapshot_path:
+#         image1 = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+#         image2 = cv2.imread(snapshot_path, cv2.IMREAD_GRAYSCALE)
+
+#         image1 = cv2.resize(image1, (RESIZE_WIDTH, RESIZE_HEIGHT))
+#         image2 = cv2.resize(image2, (RESIZE_WIDTH, RESIZE_HEIGHT))
+
+#         # Initialize ORB image1.shape == image2.shape:
+#         orb = cv2.ORB_create()
+
+#         # Detect keypoints and compute descriptors
+#         keypoints1, descriptors1 = orb.detectAndCompute(image1, None)
+#         keypoints2, descriptors2 = orb.detectAndCompute(image2, None)
+
+#         # Create a BFMatcher object
+#         matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+#         matches = matcher.knnMatch(descriptors1, descriptors2, k=2)
+
+#         # Filter good matches
+#         good_matches = []
+#         for m, n in matches:
+#             if m.distance < 0.7 * n.distance:
+#                 good_matches.append([m])
+
+#         # Draw the matches
+#         final_image = cv2.drawMatchesKnn(image1, keypoints1, image2, keypoints2, good_matches, None,
+#                                          flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+#         threshold = 10
+
+#         if len(good_matches) >= threshold:
+#             st.markdown("<div style='text-align: center; background-color: white; padding: 5px; border-radius: 5px'><strong><span style='color: black'>Images are the Match.</span></strong></div>", unsafe_allow_html=True)
+#         else:
+#             st.markdown(f"<div style='display: flex; justify-content: center;'><div style='text-align: center; background-color: white; padding: 5px 20px; border-radius: 5px; max-width: {200}px'><strong><span style='color: black'>Images don't Match.</span></strong></div></div>", unsafe_allow_html=True)
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
 import cv2
 import streamlit as st
 from PIL import Image
 
-RESIZE_WIDTH = 500
-RESIZE_HEIGHT = 300
-
-def process_uploaded_image(uploaded_file):
-    if uploaded_file is not None:
-        image = Image.open(uploaded_file)
-        # Convert RGBA to RGB if the image has an alpha channel
-        if image.mode == 'RGBA':
-            image = image.convert('RGB')
-
-        image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
-        image_path = "uploaded_image.jpg"  # Save the resized image to a file
-        image.save(image_path)
-        return image_path
-    return None
-
-def process_camera_snapshot():
-    video_capture = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
-    while video_capture.isOpened():
-        ret, frame = video_capture.read()
-        if not ret:
-            break
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image = Image.fromarray(frame)
-        image = image.resize((RESIZE_WIDTH, RESIZE_HEIGHT))
-        snapshot_path = "snapshot.jpg"  # Save the resized snapshot image to a file
-        image.save(snapshot_path)
-        return snapshot_path
-    return None
-
 def main():
-    # image = Image.open('D:\pythonProject1\MediTi.png')
-    # st.image(
-    #     image,
-    #     width=400,  # Manually adjust the width of the image as per requirement
-    #     caption="MediTi Logo"  # Add a non-empty label for accessibility
-    # # )
+    st.title("Camera Snapshot App")
 
-    st.title("Image Comparing Streamlit App")
+    # Create a button to take the snapshot
+    if st.button("Take Snapshot"):
+        # Capture an image from the camera
+        cap = cv2.VideoCapture(0+cv2.CAP_DSHOW)
+        ret, frame = cap.read()
 
-    col1, col2 = st.columns(2)
+        if ret:
+            # Convert the BGR image to RGB
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    image_path = None  # Initialize image_path variable
-    snapshot_path = None  # Initialize snapshot_path variable
+            # Display the captured image
+            st.image(frame_rgb, caption="Snapshot", use_column_width=True)
 
-    with col1:
-        st.subheader("Upload an Image")
-        uploaded_file = st.file_uploader("&nbsp", type=["jpg", "jpeg", "png"], accept_multiple_files=False, help="",label_visibility="hidden")
-        if uploaded_file is not None:
-            image_path = process_uploaded_image(uploaded_file)
+            # Save the snapshot as a file
+            snapshot_path = "snapshot.jpg"
+            cv2.imwrite(snapshot_path, frame)
 
-    with col2:
-        st.subheader("Camera Snapshot")
-        snapshot_button = st.button("Take Snapshot")
-        if snapshot_button:
-            snapshot_path = process_camera_snapshot()
-
-    # Show both images in a balanced manner
-    if image_path is not None and snapshot_path is not None:
-        st.subheader("Images")
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image(image_path, caption="Uploaded Image")
-        with col2:
-            st.image(snapshot_path, caption="Snapshot")
-
-    # Perform image comparison and print the result
-    if image_path and snapshot_path:
-        image1 = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        image2 = cv2.imread(snapshot_path, cv2.IMREAD_GRAYSCALE)
-
-        image1 = cv2.resize(image1, (RESIZE_WIDTH, RESIZE_HEIGHT))
-        image2 = cv2.resize(image2, (RESIZE_WIDTH, RESIZE_HEIGHT))
-
-        # Initialize ORB image1.shape == image2.shape:
-        orb = cv2.ORB_create()
-
-        # Detect keypoints and compute descriptors
-        keypoints1, descriptors1 = orb.detectAndCompute(image1, None)
-        keypoints2, descriptors2 = orb.detectAndCompute(image2, None)
-
-        # Create a BFMatcher object
-        matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
-        matches = matcher.knnMatch(descriptors1, descriptors2, k=2)
-
-        # Filter good matches
-        good_matches = []
-        for m, n in matches:
-            if m.distance < 0.7 * n.distance:
-                good_matches.append([m])
-
-        # Draw the matches
-        final_image = cv2.drawMatchesKnn(image1, keypoints1, image2, keypoints2, good_matches, None,
-                                         flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-        threshold = 10
-
-        if len(good_matches) >= threshold:
-            st.markdown("<div style='text-align: center; background-color: white; padding: 5px; border-radius: 5px'><strong><span style='color: black'>Images are the Match.</span></strong></div>", unsafe_allow_html=True)
         else:
-            st.markdown(f"<div style='display: flex; justify-content: center;'><div style='text-align: center; background-color: white; padding: 5px 20px; border-radius: 5px; max-width: {200}px'><strong><span style='color: black'>Images don't Match.</span></strong></div></div>", unsafe_allow_html=True)
+            st.error("Failed to capture the image from the camera. Please make sure the camera is accessible.")
+
+        # Release the camera
+        cap.release()
 
 if __name__ == "__main__":
     main()
+
 
