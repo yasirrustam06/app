8bced98d5372266ecf061190b20d23a1cbf33573
yasir ali <yasirrustam06@gmail.com>
2023-07-22 01:59:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ def process_uploaded_image(uploaded_file):
     return None
 
 def process_camera_snapshot():
-    video_capture = cv2.VideoCapture(0)
+    video_capture = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
     while video_capture.isOpened():
         ret, frame = video_capture.read()
         if not ret:
